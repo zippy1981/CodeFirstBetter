73052b40642da188f5b4e9fc90bcbfa94c7d5456
Justin Dearing <zippy1981@gmail.com>
2022-08-06 07:55:29 -0400

    Updated packages

---
+++ Zippysoft.CodeFirst.DAL/BetterDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Zippysoft.CodeFirst.DAL;
+
+/// <summary>
+/// Used by <c>dotnet ef</c> commands.
+/// </summary>
+/// <remarks>
+/// This fixture accomplishes two things. Firstly, it prevents the needs to set <c>--startup-project</c>.
+/// Secondly, we can set options here for migrations such as timeout that we don't want for the regular app.</remarks>
+public partial class BetterDbContextFactory : IDesignTimeDbContextFactory<BetterDbContext>
+{
+    /// <inheritdoc />
+    public BetterDbContext CreateDbContext(string[] args)
+    {
+        Console.WriteLine("==============  Inside BetterDbContextFactory  ==============");
+
+        var connectionString =
+            "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CodFirstBetter;Integrated Security=true;";
+        var optionsBuilder = new DbContextOptionsBuilder<BetterDbContext>();
+        
+        optionsBuilder.UseSqlServer(connectionString, opts =>
+        {
+            // Set the timeout to 5 minutes
+            opts.CommandTimeout((int)TimeSpan.FromMinutes(5).TotalSeconds);
+        });
+
+        return new BetterDbContext(optionsBuilder.Options);
+    }
+}
